4f9ea09f82bca0423e056ca56ce6f153f3ad86f3
Pablo Moreira Santos <Pablomoreirasantos.hp@gmail.com>
2024-03-10 01:23:57 -0300

    Transmartins

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,28 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "../Transmartins/assets/img/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "../Transmartins/assets/img/close_white_36dp.svg";
     }
+}
+
+const sections = document.querySelectorAll('section');
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.9 // Ajuste esse valor conforme necessário
 };
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('in-view');
+            observer.unobserve(entry.target);
+        }
+    });
+}, options);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
