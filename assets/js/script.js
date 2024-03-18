af69435caa6b810ad9572006bde25a7ca69e6533
Pablo Moreira Santos <Pablomoreirasantos.hp@gmail.com>
2024-03-18 19:36:55 -0300

    Transmartins -  Responsividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,55 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "../Transmartins/assets/img/menu.png";
     } else {
         menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "../Transmartins/assets/img/x.png";
     }
 }
 
-const sections = document.querySelectorAll('section');
+// scroll sections and footer.
+let sections = document.querySelectorAll('section');
+let footer = document.querySelector('footer');
+let navLinks = document.querySelectorAll('header nav a');
 
-const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.9 // Ajuste esse valor conforme necessário
-};
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 120;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('in-view');
-            observer.unobserve(entry.target);
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            let targetLink = document.querySelector('header nav a[href="#' + id + '"]');
+            if (targetLink) {
+                targetLink.classList.add('active');
+            }
         }
     });
-}, options);
 
-sections.forEach(section => {
-    observer.observe(section);
-});
+    // Check if in footer and update navbar
+    let footerTop = footer.offsetTop;
+    let windowHeight = window.innerHeight;
+
+    if (window.scrollY + windowHeight >= footerTop) {
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+        let contactLink = document.querySelector('header nav a[href="#contato"]');
+        if (contactLink) {
+            contactLink.classList.add('active');
+        }
+    }
+
+    // sticky navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove navbar click navbar link (scroll)
+};
+
+
